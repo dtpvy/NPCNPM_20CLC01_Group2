abcb61559bbac970a42265bea30f2ca708c9f6ca
Yuhaigiub <buigiahuynt@gmail.com>
2022-11-22 14:20:59 +0700

    create Layout

--- src/fe/src/App.jsx
+++ src/fe/src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+
+import Support from "./components/Support";
 
 const App = () => {
 	return (
 		<Routes>
-			<Route path="/" element={<Layout />}></Route>
+			<Route path="/" element={<Layout />}>
+				<Route path="support" element={<Support />} />
+			</Route>
 		</Routes>
 	);
 };
--- src/fe/src/components/Layout/index.jsx
+++ src/fe/src/components/Layout/index.jsx
@@ -4,6 +4,7 @@ import { Outlet } from "react-router-dom";
 const Layout = () => {
 	return (
 		<div>
+			<header className="w-full h-16 bg-orange-600">Header</header>
 			<Outlet />
 		</div>
 	);
