71abd366d39425b88db953cc7fd98321b911d0c1
Yuhaigiub <buigiahuynt@gmail.com>
2022-12-02 23:42:54 +0700

    change layout height

--- src/fe/src/components/Layout/index.jsx
+++ src/fe/src/components/Layout/index.jsx
@@ -5,7 +5,7 @@ const Layout = () => {
 	return (
 		<div className="h-screen w-screen">
 			<header className="h-1/6 bg-orange-600">Header</header>
-			<div className="h-5/6 bg-slate-200">
+			<div className="bg-slate-200">
 				<Outlet />
 			</div>
 		</div>
