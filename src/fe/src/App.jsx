2b9a5f1abb39a8a4e0e37d4805a29e29db19fcb7
MichaMichaHii <minhchanhwork@gmail.com>
2022-12-09 16:01:53 +0700

    profile + bill

--- src/fe/src/App.jsx
+++ src/fe/src/App.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import Login from "./components/Login";
 import Register from "./Components/Register";
+import Profile from "./Components/Profile/Profile.jsx";
+import Bought from "./Components/Profile/bought.jsx"
+import Layout from "./components/Layout";
+import EditProfile from "./Components/Profile/editProfile.jsx"
+import { Routes, Route } from "react-router-dom";
 const App = () => {
   return (
-    <Register/>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+      <Route path="bought" element={<Bought />} />
+      <Route path="profile" element={<Profile />} />
+      <Route path="editprofile" element={<EditProfile />} />
+    </Route>
+  </Routes>
   );
 };
 
