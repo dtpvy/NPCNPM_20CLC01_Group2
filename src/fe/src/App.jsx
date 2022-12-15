2d541609523c48615980d7478ed94f299ef50089
Yuhaigiub <buigiahuynt@gmail.com>
2022-12-15 17:21:29 +0700

    add Payment page

--- src/fe/src/App.jsx
+++ src/fe/src/App.jsx
@@ -15,28 +15,30 @@ import Bought from "./Components/Profile/bought.jsx";
 import EditProfile from "./Components/Profile/editProfile.jsx";
 import { useEffect } from "react";
 import { loginAPI } from "./Services/account.js";
+import Payment from "./Components/Payment/index.jsx";
 
 const App = () => {
-  useEffect(() => {
-    loginAPI({ email: "admin@gmail.com", password: "admin1" }).then((res) => {
-      console.log(res);
-    });
-  }, []);
+	useEffect(() => {
+		loginAPI({ email: "admin@gmail.com", password: "admin1" }).then((res) => {
+			console.log(res);
+		});
+	}, []);
 
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />} />
-      <Route path="bought" element={<Bought />} />
-      <Route path="profile" element={<Profile />} />
-      <Route path="editprofile" element={<EditProfile />} />
-      <Route path="home" element={<Home />} />
-      <Route path="seller-profile" element={<SellerProfile />} />
-      <Route path="support" element={<Support />} />
-      <Route path="detail/:id" element={<ProductDetail />} />
-      <Route path="order" element={<Order />} />
-      <Route path="cart-detail" element={<Cart />} />
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route path="/" element={<Layout />} />
+			<Route path="bought" element={<Bought />} />
+			<Route path="profile" element={<Profile />} />
+			<Route path="editprofile" element={<EditProfile />} />
+			<Route path="home" element={<Home />} />
+			<Route path="seller-profile" element={<SellerProfile />} />
+			<Route path="support" element={<Support />} />
+			<Route path="detail/:id" element={<ProductDetail />} />
+			<Route path="order" element={<Order />} />
+			<Route path="cart-detail" element={<Cart />} />
+			<Route path="payment" element={<Payment />} />
+		</Routes>
+	);
 };
 
 export default App;
