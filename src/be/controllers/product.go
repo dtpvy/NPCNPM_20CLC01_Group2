72f4efa681b7cdac0765af695a8fc63bdd346763
dtpvy <dtpvy0202@gmail.com>
2023-01-10 00:16:32 +0700

    update product search

--- src/be/controllers/product.go
+++ src/be/controllers/product.go
@@ -22,6 +22,7 @@ func GetProductDetail(w http.ResponseWriter, r *http.Request, ps httprouter.Para
 
 func GetProductSearch(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	search := r.URL.Query().Get("q")
+	catId := r.URL.Query().Get("category")
 	sortType := r.URL.Query().Get("st")
 	sort := r.URL.Query().Get("sort")
 	page, _ := strconv.ParseInt(r.URL.Query().Get("page"), 10, 64)
@@ -34,8 +35,8 @@ func GetProductSearch(w http.ResponseWriter, r *http.Request, ps httprouter.Para
 	}
 	order := sortType + " " + sort
 	_db := db.Connect()
-	var product md.Product
-	_db.Model(md.Product{}).Preload("Category").Preload("User").Where("name LIKE ?", "%"+search+"%").Order(order).Offset(int(page * limit)).Limit(int(limit)).Find(&product)
+	var product []presenters.Product
+	_db.Model(presenters.Product{}).Where("name LIKE ?", "%"+search+"%").Where("category_id = ?", catId).Order(order).Offset(int(page * limit)).Limit(int(limit)).Find(&product)
 	var response = presenters.BuildResponse(product)
 	json.NewEncoder(w).Encode(response)
 }
