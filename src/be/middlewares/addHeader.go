6ed7dc78d9acce15564a43e5479a072d8a9a1147
dtpvy <dtpvy0202@gmail.com>
2023-01-10 15:17:33 +0700

    add header

--- src/be/middlewares/addHeader.go
+++ src/be/middlewares/addHeader.go
@@ -10,7 +10,8 @@ func AddHeader(next httprouter.Handle) httprouter.Handle {
 	return httprouter.Handle(func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 		w.Header().Set("Content-Type", "application/json")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "origin, x-requested-with, content-type")
+		w.Header().Set("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS")
 
 		next(w, r, p)
 	})
